dd56f12d90c30c7216b580a619a688d8c947b272
jackson <woosuk2009@gmail.com>
2016-12-11 23:07:23 -0500

    url fix

--- ncbid/urls.py
+++ ncbid/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from home.views import list_artwork, view_artwork
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'home.views.list_artwork', name='list_artwork'),
-    url(r'^(?P<item_id>\d+)$', 'home.views.view_artwork', name='view_artwork'),
+    url(r'^$', list_artwork, name='list_artwork'),
+    url(r'^(?P<item_id>\d+)$', view_artwork, name='view_artwork'),
 ]
